164b13345a6f6612f45b7252dbf1d6e925215b24
ammar falmban <devAspirant@gmail.com>
2014-03-18 15:48:15 +0900

    Initial Commit

---
+++ SettingNULL_On_C/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  SettingNULL_On_C
+//
+//  Created by ammar falmban on 3/18/14.
+//  Copyright (c) 2014 SDKaizen.com. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
